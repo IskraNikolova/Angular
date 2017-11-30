6775a95a044d51fd9525258a19847dad4d7532f9
IskraNikolova <air_iskra@yahoo.com>
2017-11-30 14:44:27 +0200

    add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { seed } from './../../seed';
 
 @Component({
   selector: 'note-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'note';
+
+export class AppComponent implements OnInit{
+  data;
+  selectedArticle: Object;
+
+  ngOnInit(){
+    this.data = seed;
+  }
+
+  showDetails(targetId){
+    this.selectedArticle = this.data.find(e => e.id === targetId);
+    this.selectedArticle["view"] = 0;
+    this.selectedArticle["hide"] = false;
+    this.selectedArticle["showImage"] = false;
+  }
+
+  deleteTargetArticle(targetId){
+    this.data = this.data.filter(e => Number(e.id) !== Number(targetId));
+    this.selectedArticle = null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HeadersComponent } from './components/headers/headers.component';
+import { ArticleComponent } from './components/article/article.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadersComponent,
+    ArticleComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule
   ],
   providers: [],
